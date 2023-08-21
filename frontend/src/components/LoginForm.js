1f5bb97fac4828c330c1d407bd6ccc6c99ee0306
Austin Boone <84555510+AusBoone@users.noreply.github.com>
2023-08-21 13:23:18 -0400

    Update LoginForm.js
    - Added extensive documentation to explain the purpose of the component and the functionality of the 'handleSubmit' function.
    - Added the required attribute to the input fields to prevent form submission with empty fields.
    - Provided guidance on handling login errors and network or server errors within the code comments.

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,26 +1,42 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-// Will send a login request to the server with the provided username and password.
+/**
+ * LoginForm Component
+ * 
+ * This component handles user login by providing a form for entering username
+ * and password, and then sends a POST request to the server with the provided credentials.
+ * Upon successful login, it redirects the user to an appropriate page.
+ *
+ * @returns {React.Component} The LoginForm component
+ */
 function LoginForm() {
+  // State variables to hold the user's input for username and password
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  /**
+   * handleSubmit Function
+   * 
+   * This function is called when the login form is submitted.
+   * It sends a POST request to the login endpoint with the username and password,
+   * and handles the response accordingly.
+   *
+   * @param {Event} event - The submit event object
+   * @returns {Promise<void>}
+   */
   const handleSubmit = async (event) => {
+    // Prevent the default behavior of the form submission, e.g., page reload
     event.preventDefault();
 
     try {
+      // Send a POST request to the login endpoint with the username and password
       const response = await axios.post('https://your-api-url/login', {
         username,
         password,
       });
 
-      /*
-       If the login is successful (response status 200), 
-       you should handle storing the user session or access token securely, 
-       e.g., using cookies, localStorage, or IndexedDB, 
-       and then redirect the user to the chat page or another appropriate page.
-      */
+      // Check if the login was successful (response status 200)
       if (response.status === 200) {
         // Handle successful login, e.g., store user session or access token
         // Implement a secure storage mechanism for storing sensitive data like tokens
@@ -28,9 +44,11 @@ function LoginForm() {
         // Redirect to the chat page or another appropriate page
       } else {
         // Handle login errors
+        // You may want to update the component state or show a notification to the user
       }
     } catch (error) {
       // Handle network or server errors
+      // You may want to log the error or show an error message to the user
     }
   };
 
@@ -42,12 +60,14 @@ function LoginForm() {
         placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        required // Make this field required to prevent empty submission
       />
       <input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        required // Make this field required to prevent empty submission
       />
       <button type="submit">Login</button>
     </form>
