9b04b519dbfdababca4347b3fcdd281921ddadc7
Austin Boone <84555510+AusBoone@users.noreply.github.com>
2025-06-08 12:51:04 -0400

    Add documentation comments

--- frontend/src/components/UserAccount.js
+++ frontend/src/components/UserAccount.js
@@ -1,25 +1,16 @@
-// Importing React for creating the component
 import React from 'react';
-
-// Importing the AccountSettings component, which will handle the functionality related to updating account information
 import AccountSettings from './AccountSettings';
 
-// UserAccount component function
+/**
+ * Page container for user account management screens.
+ */
 function UserAccount() {
   return (
     <div>
-      {/* Wrapping the content inside a <div> container */}
-      {/* Header for the user account section */}
       <h2>User Account</h2>
-      {/* Rendering the AccountSettings component, responsible for updating account details */}
       <AccountSettings />
-      {/* Implement other user account management features here */}
-
-      {/* Additional components related to user account management can be added here */}
-
     </div>
   );
 }
 
-// Exporting the UserAccount component to be used in other parts of the application
 export default UserAccount;
